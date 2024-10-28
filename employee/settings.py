0474309f75bc8e59c5252a754b84f95e680b2048
SEBIN BABY <sebin.18@gmail.com>
2024-10-28 12:40:33 +0530

    minor changes

--- employee/settings.py
+++ employee/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x0z8vu@!6)^=e=)r^f^7&hz8!%57hwppi^0@5fsyl#q*t_%z7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://employeeapi-23a6ad183cd1.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['employeeapi-23a6ad183cd1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
